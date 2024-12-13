29d1363a16170a1985ee5407b1d7dd81eaca0128
Ivo Ryan <ryanbussines06@gmail.com>
2024-12-12 23:01:26 -0300

    Update

--- src/components/form/form.spec.tsx
+++ src/components/form/form.spec.tsx
@@ -1,11 +1,16 @@
 import { render, screen } from '@testing-library/react';
-import { it, describe } from 'vitest';
+import { it, describe, expect } from 'vitest';
 import { Form } from '.';
+import '@testing-library/jest-dom';
 
 describe("<Form/>", () => {
     it('should render in the document', async () => {
         render(<Form/>);
 
-        const input = screen.getAllByRole('textbox');
-    })
-})
+        const inputUser = screen.getAllByRole('textbox', {name: 'user'});
+        const inputEmail = screen.getByRole('texbox', {name: 'email'});
+
+        expect(inputUser).toBeVisible();
+        expect(inputEmail).toBeVisible();
+    });
+});
