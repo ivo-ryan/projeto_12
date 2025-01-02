ce8914a44e924d3e6a5117acd5b95164c74ca439
Ivo Ryan <ryanbussines06@gmail.com>
2025-01-01 22:28:43 -0300

    Update

--- src/components/menu/style.tsx
+++ src/components/menu/style.tsx
@@ -6,7 +6,6 @@ export const Header = styled.header`
     background-color: #3F3D3C;
     color: #fff;
     width: 100%;
-    padding: 0 3rem;
     height: 7rem;
     box-shadow: 0 .5rem 1.4rem #111;
 `;
@@ -16,6 +15,7 @@ export const Menu = styled.ul`
     justify-content: space-between;
     align-items: center;
     height: 100%;
+    padding: 0 3rem;
 
     li + li {
         display: flex;
