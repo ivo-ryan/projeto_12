ed3278d3596eac99d1c7b790e2f542cd0440ea49
Ivo Ryan <ryanbussines06@gmail.com>
2025-01-02 21:15:08 -0300

    Update

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -9,9 +9,9 @@ export const Footer = () => {
                 <a href="/privacy-policy" >Política de Privacidade</a>
             </S.Container>
             <S.Contact>
-                <S.WhatsContainer><a href="/"><FaWhatsapp/></a></S.WhatsContainer>
-                <S.TelegContainer><a href="/"><FaTelegram/></a></S.TelegContainer>
-                <S.LinkedContainer> <a href="/"><FaLinkedin/></a></S.LinkedContainer>
+                <S.WhatsContainer><a href="https://wa.me/5563992644269?text=OI%C3%A1%20Ryan%20,%20acabei%20vendo%20seu%20perfil%20no%20github%20e%20queria%20conversar!" target='_blank'><FaWhatsapp/></a></S.WhatsContainer>
+                <S.TelegContainer><a href="https://t.me/ivo_ryan" target='_blank'><FaTelegram/></a></S.TelegContainer>
+                <S.LinkedContainer> <a href="https://www.linkedin.com/in/ryan-bastos-a0a8262a7/" target='_blank' ><FaLinkedin/></a></S.LinkedContainer>
             </S.Contact>
         </S.Footer>
     )
